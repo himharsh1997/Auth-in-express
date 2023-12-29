0db1f88af19444f401415f53a90b5b839888f230
himanshufize <himanshu.joshi@getfize.com>
2023-12-29 18:03:31 +0530

    update appjs

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-require('dotenv').config(); // Configure environment varaiable value of current node instance env object
+// require('dotenv').config(); // Configure environment varaiable value of current node instance env object
 
 const express = require('express');
-const logger = require('morgan');
+// const logger = require('morgan');
 const bodyParser = require('body-parser');
-const config = require('config');
+// const config = require('config');
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
-const environment = process.env.NODE_ENV; // Development not but on run can be set to Production based on production environment
+// const environment = process.env.NODE_ENV; // Development not but on run can be set to Production based on production environment
 
 app.use(bodyParser.json());    // Using middleware bodyParser to populate body with data
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const routes = require('./routes/index.routes');
-app.use('/app/v1',routes(router));
+app.get('/status', (req, res) => {
+  return res.send({ status: 'OK' })
+})
+
+// const routes = require('./routes/index.routes');
+// app.use('/app/v1',routes(router));
 
 const PORT = 3001;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
